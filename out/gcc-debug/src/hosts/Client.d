5ee33c737b66fc4689928e01d88ea6ce0a6e7b0e
Timo Haeckel <Timo.Haeckel@haw-hamburg.de>
2017-10-13 09:45:06 +0200

    Updated Project setup it now supports core4inet librarys and inet itself... Also implemented an example project

--- out/gcc-debug/src/hosts/Client.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/hosts/Client.o: hosts/Client.cpp hosts/Client.h \
- hosts/Host.h C:/omnetpp-5.1.1/include/omnetpp.h
-
-hosts/Client.h:
-
-hosts/Host.h:
-
-C:/omnetpp-5.1.1/include/omnetpp.h:
--- out/gcc-debug/src/hosts/Server.d
+++
@@ -1,8 +0,0 @@
-../out/gcc-debug/src/hosts/Server.o: hosts/Server.cpp hosts/Server.h \
- hosts/Host.h C:/omnetpp-5.1.1/include/omnetpp.h
-
-hosts/Server.h:
-
-hosts/Host.h:
-
-C:/omnetpp-5.1.1/include/omnetpp.h:
