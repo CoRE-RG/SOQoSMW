e574854f52ed320e4c8727e9fd2f1ff20ac9c49a
Timo Haeckel <Timo.Haeckel@haw-hamburg.de>
2017-12-01 10:47:20 +0100

    Added Endpoints for subscriber and publisher

---
+++ src/soqosmw/endpoints/publisher/standard/tcp/TCPPublisher.cc
@@ -0,0 +1,30 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include <endpoints/publisher/standard/tcp/TCPPublisher.h>
+
+namespace soqosmw {
+
+TCPPublisher::TCPPublisher() {
+    // TODO Auto-generated constructor stub
+
+}
+
+TCPPublisher::~TCPPublisher() {
+    // TODO Auto-generated destructor stub
+}
+
+} /*end namespace soqosmw*/
+
---
+++ src/soqosmw/endpoints/subscriber/realtime/avb/AVBSubscriber.h
@@ -0,0 +1,31 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef SOQOSMW_ENDPOINTS_SUBSCRIBER_REALTIME_AVB_AVBSUBSCRIBER_H_
+#define SOQOSMW_ENDPOINTS_SUBSCRIBER_REALTIME_AVB_AVBSUBSCRIBER_H_
+
+#include <endpoints/subscriber/realtime/base/IRTSubscriber.h>
+
+namespace soqosmw {
+
+class AVBSubscriber: public IRTSubscriber {
+public:
+    AVBSubscriber();
+    virtual ~AVBSubscriber();
+};
+
+} /*end namespace soqosmw*/
+
+#endif /* SOQOSMW_ENDPOINTS_SUBSCRIBER_REALTIME_AVB_AVBSUBSCRIBER_H_ */
